7a8c9ae37839c961b014b3087c990968559cf0b0
PAT CAMP <PATS0825@MSN.COM>
2018-11-28 09:45:26 -0500

    updated my welcome index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :pets
   root 'welcome#index'
   get 'patspets' => 'welcome#index'
   get 'about_pat' =>'welcome#about'
+  get 'contact_pat' => 'welcome#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
